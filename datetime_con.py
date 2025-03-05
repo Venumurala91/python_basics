7f059cb8ed0c13354433d6785a7eab66a6d3f6bf
Venu <muralavenu24@gmail.com>
2025-03-05 18:33:02 +0530

    Learned datetime concepts

---
+++ datetime_con.py
@@ -0,0 +1,21 @@
+# 1. date Module
+        # The date module in Python is part of the datetime library,
+        # which allows you to work with dates (year, month, and day).
+        # The date class in the datetime module is used to manipulate dates.
+
+
+from datetime import date
+
+# Getting the current date
+today = date.today()
+print("Today's date:", today)  # Output: 2025-03-05 (current date)
+
+# Accessing individual parts of the date
+print("Year:", today.year)  # Output: 2025
+print("Month:", today.month)  # Output: 3
+print("Day:", today.day)  # Output: 5
+
+
+
+
+
