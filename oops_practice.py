9bf28377127afb9f3dd84010f56cdeed69a45726
Venu <muralavenu24@gmail.com>
2025-03-05 18:03:01 +0530

    added some more topics

--- oops_practice.py
+++ oops_practice.py
@@ -264,5 +264,33 @@ print(dog.get_info())
 
 # ----------------------------------------------------------------------------------------------------><-------------------------------------------------------------------
 
-# Data abstraction :It Hides the internal implementation details and shows only neccessary information
+
+
+
+# super() funtion in python
+#
+# super method is used to call parent class to use thier keywords 0r attributes
+# ->Also it is used to create additional fields in child classes
+#
+# ex:
+
+class Emp():
+    def __init__(self, id, name, Add):
+        self.id = id
+        self.name = name
+        self.Add = Add
+
+# Class freelancer inherits EMP
+class Freelance(Emp):
+    def __init__(self, id, name, Add, Emails):
+        super().__init__(id, name, Add)
+        self.Emails = Emails
+
+Emp_1 = Freelance(103, "Suraj kr gupta", "Noida" , "KKK@gmails")
+print('The ID is:', Emp_1.id)
+print('The Name is:', Emp_1.name)
+print('The Address is:', Emp_1.Add)
+print('The Emails is:', Emp_1.Emails)
+
+
 
