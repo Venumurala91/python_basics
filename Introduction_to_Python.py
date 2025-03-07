0f2eea49f1136b805640f538974637c19716abdd
Venu <muralavenu24@gmail.com>
2025-03-07 11:33:46 +0530

    Introduction to python

---
+++ Introduction_to_Python.py
@@ -0,0 +1,72 @@
+# from turtledemo.nim import computerzug
+#
+# What is Python?
+# Python is a popular high level programming language,object oriented,interpreted  and dynamically typed language . It was created by Guido van Rossum, and released in 1991.
+#
+#
+# # Python is an object-oriented programming language because it uses classes and objects to define and manage data. This makes programs easier to understand and maintain.
+# # How Python uses objects and classes
+# #     Classes: blue print to create an object
+# #     Objects:Instance of a class
+# #         Represent real-world entities. Objects can have properties, methods, and types.
+# #     Benefits of object-oriented programming
+# #         Reusable code
+# #         Child classes can inherit attributes and methods from parent classes, reducing duplication.
+# #         Modular code
+# #         Encapsulation bundles data and behaviors into classes, promoting modular code.
+# #         Maintainable code
+# #         Abstraction hides implementation details, making it easier to focus on what an object does.
+#
+# # why python is interpreted langauge :
+# #     it executed the code line by line and throws error at runtime only so it is interpreted
+# #
+# # In other languages like java , c , c++ we can use compiler
+# #     compiler will executes entire code and throws error at a time once all.
+#
+# # Python is dynamically typed language because : No need to declare data type while we are assigning values to a variable
+#     # ex: In python
+#     #     a=5    #not giving any data type
+#     #     print(a)
+#     # ex: In java
+#     #     int a =5
+#     #     system.print.ln(a)
+#
+#
+#
+# It is used for:
+#
+#             web development (server-side),
+#             software development,
+#             Data science , Machine learning
+#             mathematics,
+#             system scripting
+#
+#
+#
+# # what python do :
+# #         Python is a versatile programming language used to build websites, analyze data, automate tasks, and more.
+# #         It's a general-purpose language that's easy to learn and use, making it a good choice for beginners.
+#
+#
+#
+# # how python code executes in system or computer
+#
+#
+# # python source code (first.py) --> Byte code(.pyc(compiled ))--machine code(10101100110)-->output(computer)
+#
+# # Why Python?
+#     # Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+#     # Python has a simple syntax similar to the English language.
+#     # Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+#     # Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+#     # Python can be treated in a procedural way, an object-oriented way or a functional way.
+#
+# print("Hello, World!")
+# output: Hello, World!
+
+
+#This is a comment
+print("Hello, World!")
+
+# Variables
+# Variables are containers for storing data values.
