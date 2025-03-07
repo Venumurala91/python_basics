c4746bd02171011916e515323d2ded140e3eee09
Venu <muralavenu24@gmail.com>
2025-03-07 22:07:39 +0530

    tast_2

--- Oops_task2(banking).py
+++ Oops_task2(banking).py
@@ -1,37 +1,43 @@
 class bankaccount:
-    def __init__(self,account_number,balance,amount):
+    def __init__(self,account_number,balance):
         self.account_number=account_number
         self.balance=balance
-        self.amount=amount
+        # self.amount=amount
 
 
     def deposit(self,amount):
-        total_balance= self.balance+self.amount
+        total_balance= self.balance+amount
         self.balance=total_balance
         print(self.balance)
         print(f"added money ",total_balance)
-
+        return ''
 
     def withdraw(self,amount):
-        withdraw_amount= self.balance- self.amount
+        withdraw_amount= self.balance- amount
+        self.balance=withdraw_amount
         # print(self.balance)
         if withdraw_amount <=0:
             print("balance is insufficient")
         else:
-            print("withdrawll_amount:",str(withdraw_amount))
+            print("withdrawll_amount:",amount)
+            print("Remaining_balance_amount:",self.balance)
+
+        return ''
 
 
     def display_balance(self):
-        print(f"current_balance",{self.balance})
-
-
-
-
-
+        print(f"current_balance",self.balance)
+        return ''
 
 class customer:
     pass
 
-b=bankaccount("12345678910",2000,500)
-print(b.deposit(500))
+b=bankaccount("12345678910",2000)
+print(b.deposit(501))
 print(b.withdraw(250))
+print(b.display_balance())
+
+
+
+
+
